c79e7294998bd1d112b5a62b8ed72a09285ff8c9
Sooraj <soorajnambiar8@gmail.com>
2023-09-20 22:00:27 +0530

    initial commit

--- index.js
+++ index.js
@@ -20,12 +20,12 @@ app.get("/home", (req, res) => {
     res.render("home.ejs")
 })
 
-app.get("/log", (req, res) => {
-    res.render("log.ejs")
-})
-
 app.get("/sign", (req, res) => {
     res.render("sign.ejs")
+})
+
+app.get("/log", (req, res) => {
+    res.render("log.ejs")
 })
 
 app.get("/mate", (req, res) => {
@@ -43,6 +43,45 @@ app.get("/travel", (req, res) => {
 app.get("/study", (req, res) => {
     res.render("study.ejs")
 })
+app.post('/datasignin', function(req,res){
+
+    let name=req.body.name;
+    let email=req.body.email;
+    let password=req.body.password;
+    let regno=req.body.password;
+    let year=req.body.year;
+    res.write('You sent the name "' ,name,'".\n');
+    res.write('You sent the email "' + req.body.email+'".\n');
+    res.write('You sent the username "' + req.body.username+'".\n');
+    res.write('You sent the username "' + req.body.username+'".\n');
+    res.write('You sent the username "' + req.body.username+'".\n');
+    
+    con.connect(function(err) {
+    if (err) throw err;
+    // let sql = "INSERT INTO users (name, email,description) VALUES ('"+name+"', '"+email+"','"+username+"')";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("1 record inserted");
+       res.end();
+    });
+    });
+  })
+  
+
+data =[]
+
+
+// try{
+    
+// q = `insert into users values(?,?,?,?,?)`
+
+// connection.query(q, data ,(err, result) => {
+//     if (err) throw err;
+//     console.log(result);
+// });
+// }catch (err){
+// console.log(err);
+// }
 
 app.listen("8080", () => {
     console.log("Server is running")
